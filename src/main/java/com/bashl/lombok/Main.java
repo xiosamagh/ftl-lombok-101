3a828fb5891499c81181ecb92523f453b68d5251
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-17 19:19:47 +0300

    task7 - ToString example

--- src/main/java/com/bashl/lombok/Main.java
+++ src/main/java/com/bashl/lombok/Main.java
@@ -31,6 +31,10 @@ public class Main {
         getterSetterExample.setName("Dota");
 
         System.out.println(getterSetterExample);
+
+        ToStringExample toStringExample = new ToStringExample();
+
+        System.out.println(toStringExample.toString());
     }
 
 
