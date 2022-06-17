4e5850a14ee2df560e82be6309f8fc76dff18249
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-17 18:07:35 +0300

    task2 - val example

--- src/main/java/com/bashl/lombok/Main.java
+++ src/main/java/com/bashl/lombok/Main.java
@@ -1,7 +1,21 @@
 package com.bashl.lombok;
 
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("d");
+
+        ValExample valExample = new ValExample();
+
+        System.out.println(valExample.example());
+
+        valExample.example2();
     }
+
+
 }
+
+
